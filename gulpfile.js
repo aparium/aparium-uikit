52db43f6d8c87d20fbd840b2cb09a93d6288bf0c
Gary Stringham <garystringham@aparium.com>
2024-12-28 02:27:02 -0800

    Updated Gulp FIle

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,10 @@ const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const replace = require('gulp-replace');
 const notify = require('gulp-notify');
-const rev = require('gulp-rev');
+
 
 // Dynamic Import for ESM Modules
+const loadRev = async () => (await import('gulp-rev')).default;
 const loadDel = async () => (await import('del')).deleteSync;
 
 const outputDir = './dist';
@@ -26,49 +27,53 @@ const handleError = (error) => {
 };
 
 // Clean Task
-gulp.task('clean', () => {
-    return del([outputDir]);
+gulp.task('clean', async () => {
+    const { deleteSync } = await import('del'); // Dynamically import del
+    deleteSync([outputDir]); // Use deleteSync to clean the directory
 });
 
+
 // Compile LESS to CSS and Minify
-gulp.task('css', () => {
+gulp.task('css', async () => {
+    const rev = await loadRev();
     return gulp
-        .src(paths.css)
-        .pipe(less().on('error', handleError)) // Handle LESS compilation errors
-        .pipe(cleanCSS().on('error', handleError)) // Handle CSS minification errors
-        .pipe(concat('uikit.aparium.theme.min.css'))
-        .pipe(rev()) // Add version hash
-        .pipe(gulp.dest(`${outputDir}/css`))
-        .pipe(rev.manifest('rev-manifest.json', { merge: true }))
-        .pipe(gulp.dest(outputDir))
-        .pipe(notify({ message: 'CSS task complete', onLast: true }));
+    .src(paths.css)
+    .pipe(less().on('error', handleError))
+    .pipe(cleanCSS().on('error', handleError))
+    .pipe(concat('uikit.aparium.theme.min.css'))
+    .pipe(rev()) // Add version hash
+    .pipe(gulp.dest(`${outputDir}/css`))
+    .pipe(rev.manifest('rev-manifest.json', { merge: true }))
+    .pipe(gulp.dest(outputDir))
+    .pipe(notify({ message: 'CSS task complete', onLast: true }));
 });
 
 // Minify JS
-gulp.task('js', () => {
+gulp.task('js', async () => {
+    const rev = await loadRev();
     return gulp
-        .src(paths.js)
-        .pipe(uglify().on('error', handleError)) // Handle JS minification errors
-        .pipe(concat('app.min.js'))
-        .pipe(rev()) // Add version hash
-        .pipe(gulp.dest(`${outputDir}/js`))
-        .pipe(rev.manifest('rev-manifest.json', { merge: true }))
-        .pipe(gulp.dest(outputDir))
-        .pipe(notify({ message: 'JS task complete', onLast: true }));
+    .src(paths.js)
+    .pipe(uglify().on('error', handleError))
+    .pipe(concat('app.min.js'))
+    .pipe(rev()) // Add version hash
+    .pipe(gulp.dest(`${outputDir}/js`))
+    .pipe(rev.manifest('rev-manifest.json', { merge: true }))
+    .pipe(gulp.dest(outputDir))
+    .pipe(notify({ message: 'JS task complete', onLast: true }));
 });
 
 // Update Theme References in HTML
 gulp.task('html', () => {
     return gulp
-        .src(paths.html)
-        .pipe(replace('../dist/css/uikit-core.css', 'https://cdn.jsdelivr.net/gh/aparium/css-style/css/uikit.aparium.theme.min.css').on('error', handleError)) // Handle HTML replace errors
-        .pipe(gulp.dest(outputDir))
-        .pipe(notify({ message: 'HTML task complete', onLast: true }));
+    .src(paths.html)
+    .pipe(replace('../dist/css/uikit-core.css', 'https://cdn.jsdelivr.net/gh/aparium/css-style/css/uikit.aparium.theme.min.css').on('error', handleError))
+    .pipe(gulp.dest(outputDir))
+    .pipe(notify({ message: 'HTML task complete', onLast: true }));
 });
 
 // Watch for Changes
 gulp.task('watch', () => {
-    const watchOptions = { delay: 500 }; // Debounce time in milliseconds
+    const watchOptions = { delay: 500 };
     gulp.watch(paths.js, watchOptions, gulp.series('js'));
     gulp.watch(paths.html, watchOptions, gulp.series('html'));
     gulp.watch(paths.css, watchOptions, gulp.series('css'));
@@ -76,8 +81,7 @@ gulp.task('watch', () => {
 
 const isCI = process.env.CI === 'true'; // Check if running in CI/CD environment
 
-// Build Task
+// Build Task    
 gulp.task('build', gulp.series('clean', 'css', 'js', 'html'));
-
 // Default Task
 gulp.task('default', gulp.series('build', isCI ? () => {} : 'watch'));
