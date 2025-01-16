7aed8bd74d3a2b697a14f27217a4dccad135a0b1
Gary Stringham <garystringham@aparium.com>
2025-01-15 16:16:15 -0800

    Commit

--- gulpfile.js
+++ gulpfile.js
@@ -73,6 +73,76 @@ gulp.task('html', () => {
     .pipe(notify({ message: 'HTML task complete', onLast: true }));
 });
 
+var realFavicon = require ('@realfavicongenerator/gulp-real-favicon');
+var fs = require('fs');
+
+// File where the favicon markups are stored
+var FaviconDataFile = 'faviconData.json';
+
+// Generate the icons.
+gulp.task('generate-favicon', function(done) {
+	realFavicon.generateFavicon({
+		masterPicture: './src/icons/aparium_mark_black.png',
+		dest: './dist',
+		iconsPath: '/favicon/',
+		design: {
+			"desktop": {
+				"darkIconTransformation": {
+					"type": "none",
+					"backgroundColor": "#ffffff",
+					"backgroundRadius": 0.7,
+					"imageScale": 0.7,
+					"brightness": 1
+				},
+				"darkIconType": "specific",
+				"regularIconTransformation": {
+					"type": "none",
+					"backgroundColor": "#ffffff",
+					"backgroundRadius": 0.7,
+					"imageScale": 0.7,
+					"brightness": 1
+				}
+			},
+			"touch": {
+				"transformation": {
+					"type": "background",
+					"backgroundColor": "#ffffff",
+					"backgroundRadius": 0,
+					"imageScale": 0.6,
+					"brightness": 1
+				},
+				"appTitle": "Aparium"
+			},
+			"webAppManifest": {
+				"transformation": {
+					"type": "background",
+					"backgroundColor": "#ffffff",
+					"backgroundRadius": 0,
+					"imageScale": 0.5,
+					"brightness": 1
+				},
+				"name": "Aparium",
+				"shortName": "Aparium",
+				"backgroundColor": "#ffffff",
+				"themeColor": "#ffffff"
+			}
+		},
+		markupFile: FaviconDataFile
+	}, function() {
+		done();
+	});
+});
+
+// Inject the favicon markups in your HTML pages. You should run
+// this task whenever you modify a page. You can keep this task
+// as is or refactor your existing HTML pipeline.
+gulp.task('inject-favicon-markups', function() {
+	return gulp.src([ './dist/*.html' ])
+		.pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FaviconDataFile)).favicon.html_code))
+		.pipe(gulp.dest(outputDir));
+});
+
+
 // Watch for Changes
 gulp.task('watch', () => {
     const watchOptions = { delay: 500 };
